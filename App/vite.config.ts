3e91cece9c2fe56164ffb9b6863acf9629fbfa11
Irvin Bernardo <irvin@trustero.com>
2025-12-12 17:25:20 +0800

    fixed pathing

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/spoology/" : "/",
+  base: "/",
   build: {
     outDir: "dist",
   },
